237e06cce536d8ecd4f18cbd5b443ea9147ef2e1
js_heo <js_heo@hancomum.com>
2024-07-09 15:36:08 +0900

    [Vite] Add Server Proxy For REST Request

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,23 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:10520',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '/api'),
+      },
+    },
+    port: 4080
+  },
+  resolve: {
+    alias: {
+      // app: resolve(__dirname, "src", "app"),
+      // components: resolve(__dirname, "src", "components"),
+      // hooks: resolve(__dirname, "src", "hooks"),
+      // "@": path.resolve(__dirname, "./src"),
+    },
+  },
+
 })
